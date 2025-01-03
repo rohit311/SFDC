55e4f04884826dceab9434515f7ecb6331a3b290
rohit369 <scorpiorohit311@gmail.com>
2025-01-03 15:12:36 +0530

    Add support for query job details

--- sfdc-js-library/src/oauth/bulk2.js
+++ sfdc-js-library/src/oauth/bulk2.js
@@ -33,3 +33,28 @@ const createQueryJob = async (instanceUrl, query) => {
     console.log("error:", error);
   }
 };
+
+const getQueryJobInfo = async (instanceUrl, queryJobId) => {
+  if (!instanceUrl || !queryJobId) {
+    throw new Error("Empty instance url or query job id !");
+  }
+
+  try {
+    const result = fetch(`${instanceUrl}/services/data/v62.0/jobs/query/${queryJobId}`, {
+      method: 'GET'
+    });
+
+    const response = await result.json();
+
+    return response;
+
+  } catch(error) {
+    // handle error
+    console.log("error:", error);
+  }
+};
+
+export {
+  createQueryJob,
+  getQueryJobInfo
+};
